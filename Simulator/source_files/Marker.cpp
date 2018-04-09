0d8c7aeb7d37fc0468056f5ea92881376e0e6064
BryannX <bngugi13@alaalumni.org>
2018-04-09 23:11:27 +0200

    Added code to marker class

--- Simulator/source_files/Marker.cpp
+++ Simulator/source_files/Marker.cpp
@@ -4,6 +4,19 @@
 
 #include "Marker.h"
 
+/* Color 0 represents red bugs
+ * Color 1 represents black bugs
+ * 
+ * To ensure the uniqueness of of mark and color combinations, the first
+ * set of combinations will be assigned to red bugs and run between
+ * 0-5. 
+ * 
+ * For black bugs, the set of combinations will be assigned to black bugs 
+ * and run between 6-11.
+ */
+
+using namespace std;
+
 
 Marker::Marker(){
 
@@ -14,18 +27,65 @@ Marker::~Marker(){
 };
 
 Marker::set_marker(tmark mark, tcolor col){
+    if(mark.mark <0 || mark.mark > 5){
+        
+        cerr << "Invalid mark input!" << endl;    
+        
+    }else if(col.color != 0 && col.color!= 1){
+        
+        cerr << "Invalid color input!" <<endl;
+        
+    }else{
+    
+        if(col.color == 0){
+            this->bits = mark.mark;
+        }else{
+            this->bits = mark.mark + 6;
+        }
+        
+    }
 
 };
 
 Marker::clear_marker(tmark mark, tcolor col){
+    if(mark.mark <0 || mark.mark > 5){
+        cerr << "Invalid mark input!" << endl;        
+    }
+    
+    if(col.color != 0 && col.color!= 1){
+        
+        cerr << "Invalid color input!" <<endl;
+        
+    }else{
+    
+        this->bits= -1;
+    
+    }
 
 };
 
 Marker::check_marker(tmark mark, tcolor col){
+        if(mark.mark <0 || mark.mark > 5){
+        cerr << "Invalid mark input!" << endl;        
+    }
+    
+    if(col.color != 0 && col.color!= 1){
+        
+        cerr << "Invalid color input!" <<endl;
+        
+    }else{
+    
+        return this->bits == mark.mark || this->bits == mark.mark + 6;
+        
+    }
 
 };
 
 Marker::check_other_marker(tcolor col){
+    
+    if(col.color != 0 && col.color!= 1){
+        cerr << "Invalid color input!" <<endl;
+    }
 
 };
 
--- Simulator/source_files/test_marker.cpp
+++ Simulator/source_files/test_marker.cpp
@@ -12,6 +12,8 @@
  */
 
 #include <cstdlib>
+#include "Marker.h"
+#include "allClasses.h"
 
 using namespace std;
 
@@ -20,13 +22,29 @@ using namespace std;
  */
 
 
-
 int main(int argc, char** argv) {
     
-    //Test for Marker
+    Marker one();
     
+    //Proper Input
+    one.set_marker(0, 1);
+    one.clear_marker(0, 1);
+    one.check_marker(0, 1);
+    one.check_other_marker(1);
     
-
+    //Erroneous Input
+    one.set_marker(7, 7);
+    one.clear_marker(7, 7);
+    one.check_marker(7, 7);
+    one.check_other_marker(7);
+    
+    //Mixed Input
+    one.set_marker(0, 7);
+    one.clear_marker(0, 7);
+    one.check_marker(0, 7);
+    one.check_other_marker(7);
+    
+        
     return 0;
-}
 
+}
